6880469b5c0de6b73268da73713ac1c6b6bc9da2
RashellChandler <rashell.chandler@yahoo.com>
2023-08-26 14:27:38 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Number Guessing Game
+var r = new Random();
+var favNumber = r.Next(0, 1001);
+
+Console.WriteLine("Pick a number from 1-1000");
+var userInput = int.Parse(Console.ReadLine());
+
+if(userInput < favNumber)
+{
+    Console.WriteLine($"{userInput} is too low");
+}
+else if(userInput > favNumber)
+{
+    Console.WriteLine($"{userInput} is too high");
+}
+else
+{
+    Console.WriteLine("You guessed it!");
+
+}
